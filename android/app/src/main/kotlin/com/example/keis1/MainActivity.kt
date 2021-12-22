19fe345a592ffa21b93b9801c08a35e66e413b12
artemcheg <artemchegushka69@gmail.com>
2021-12-22 22:15:05 +0300

    Калькулятор пиццы

---
+++ android/app/src/main/kotlin/com/example/keis1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.keis1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:keis1/pizza.dart';
+
+void main() {
+  runApp(const Pizza());
+}
+
+
+
